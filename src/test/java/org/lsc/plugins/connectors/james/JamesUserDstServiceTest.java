63d1536a8bb427e18ec3062fbfdab80535f5aefa
Quan Tran <hqtran@linagora.com>
2022-04-20 11:39:19 +0700

    Update outdated Tmail image

--- src/test/java/org/lsc/plugins/connectors/james/JamesUserDstServiceTest.java
+++ src/test/java/org/lsc/plugins/connectors/james/JamesUserDstServiceTest.java
@@ -95,7 +95,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 
 public class JamesUserDstServiceTest {
-    private static final URL PRIVATE_KEY = ClassLoader.getSystemResource("conf/private.pem");
+    private static final URL PRIVATE_KEY = ClassLoader.getSystemResource("conf/jwt_privatekey");
     private static final URL PUBLIC_KEY = ClassLoader.getSystemResource("conf/jwt_publickey");
     private static final String DOMAIN = "james.org";
     public static final String USER = "bob@" + DOMAIN;
@@ -109,10 +109,11 @@ public class JamesUserDstServiceTest {
 
     @BeforeAll
     static void setup() throws Exception {
-        james = new GenericContainer<>("linagora/james-memory:tmail-0.2.0");
+        james = new GenericContainer<>("linagora/tmail-backend:memory-branch-master");
         String webadmin = ClassLoader.getSystemResource("conf/webadmin.properties").getFile();
         james.withExposedPorts(JAMES_WEBADMIN_PORT)
             .withFileSystemBind(PUBLIC_KEY.getFile(), "/root/conf/jwt_publickey", BindMode.READ_ONLY)
+            .withFileSystemBind(PRIVATE_KEY.getFile(), "/root/conf/jwt_privatekey", BindMode.READ_ONLY)
             .withFileSystemBind(webadmin, "/root/conf/webadmin.properties", BindMode.READ_ONLY)
             .start();
 
@@ -185,7 +186,7 @@ public class JamesUserDstServiceTest {
     }
 
     @Test
-    void jamesUserListShouldReturnEmptyWhenNoUser() throws Exception {
+    void jamesUserListShouldReturnEmptyWhenNoUser() {
         with()
             .get("")
         .then()
